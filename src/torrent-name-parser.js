6369cb68b5a07dde82706191f613198d93744650
Leeroy Brun <leeroy.brun@gmail.com>
2016-06-14 17:01:27 +0200

    Initial commit

---
+++ src/torrent-name-parser.js
@@ -0,0 +1,389 @@
+/*jshint multistr: true */
+
+import makeDebug from 'debug';
+import XRegExp from 'xregexp';
+
+const debug = makeDebug('torrents-search:name-parser');
+
+const Quality = {
+  Unknown:      { name: null, weight: 1 },
+  SDTV:         { name: 'SDTV', weight: 2 },
+  DVD:          { name: 'DVD', weight: 3 },
+  WEBDL1080p:   { name: 'WEBDL-1080p', weight: 4 },
+  HDTV720p:     { name: 'HDTV-720p', weight: 5 },
+  WEBDL720p:    { name: 'WEBDL-720p', weight: 6 },
+  Bluray720p:   { name: 'Bluray-720p', weight: 7 },
+  Bluray1080p:  { name: 'Bluray-1080p', weight: 8 },
+  WEBDL480p:    { name: 'WEBDL-480p', weight: 9 },
+  HDTV1080p:    { name: 'HDTV-1080p', weight: 10 },
+  RawHD:        { name: 'Raw-HD', weight: 11 },
+  HDTV2160p:    { name: 'HDTV-2160p', weight: 12 },
+  WEBDL2160p:   { name: 'WEBDL-2160p', weight: 13 },
+  Bluray2160p:  { name: 'DVD', weight: 14 }
+};
+
+const Resolution = {
+  _480p: '480p',
+  _576p: '576p',
+  _720p: '720p',
+  _1080p: '1080p',
+  _2160p: '2160p',
+  Unknown: null
+};
+
+const Language = {
+  Unknown: null,
+  English: 'English',
+  French: 'French',
+  Spanish: 'Spanish',
+  German: 'German',
+  Italian: 'Italian',
+  Danish: 'Danish',
+  Dutch: 'Dutch',
+  Japanese: 'Japanese',
+  Cantonese: 'Cantonese',
+  Mandarin: 'Mandarin',
+  Russian: 'Russian',
+  Polish: 'Polish',
+  Vietnamese: 'Vietnamese',
+  Swedish: 'Swedish',
+  Norwegian: 'Norwegian',
+  Finnish: 'Finnish',
+  Turkish: 'Turkish',
+  Portuguese: 'Portuguese',
+  Flemish: 'Flemish',
+  Greek: 'Greek',
+  Korean: 'Korean',
+  Hungarian: 'Hungarian'
+};
+
+// Inspired from https://github.com/Sonarr/Sonarr/blob/develop/src/NzbDrone.Core/Parser/QualityParser.cs
+const REGEX = {
+  source: XRegExp('\b(?:\
+    (?<bluray>BluRay|Blu-Ray|HDDVD|BD)|\
+    (?<webdl>WEB[-_. ]DL|WEBDL|WebRip|iTunesHD|WebHD|[. ]WEB[. ](?:[xh]26[45]|DD5[. ]1)|\d+0p\.WEB\.)|\
+    (?<hdtv>HDTV)|\
+    (?<bdrip>BDRip)|\
+    (?<brrip>BRRip)|\
+    (?<dvd>DVD|DVDRip|NTSC|PAL|xvidvd)|\
+    (?<dsr>WS[-_. ]DSR|DSR)|\
+    (?<pdtv>PDTV)|\
+    (?<sdtv>SDTV)|\
+    (?<tvrip>TVRip)\
+  )\b', 'gix'),
+  rawHD: XRegExp('\b(?<rawhd>RawHD|1080i[-_. ]HDTV|Raw[-_. ]HD|MPEG[-_. ]?2)\b', 'gi'),
+  proper: XRegExp('\b(?<proper>proper|repack|rerip)\b', 'gi'),
+  version: XRegExp('\dv(?<version>\d)\b|\[v(?<version>\d)\]', 'gi'),
+  real: XRegExp('\b(?<real>(?:REAL[-_. ])+(?:PROPER|REPACK|RERIP)|(?:PROPER|REPACK|RERIP)(?:[-_. ]REAL)+)\b', 'gi'),
+  resolution: XRegExp(String.raw`\b(?:(?<_480p>480p|640x480|848x480)|(?<_576p>576p)|(?<_720p>720p|1280x720)|(?<_1080p>1080p|1920x1080)|(?<_2160p>2160p))\b`, 'gi'),
+  codec: XRegExp('\b(?:(?<x264>x264)|(?<h264>h264)|(?<xvidhd>XvidHD)|(?<xvid>Xvid)|(?<divx>divx))\b', 'gi'),
+  otherSource: XRegExp('(?<hdtv>HD[-_. ]TV)|(?<sdtv>SD[-_. ]TV)', 'gi'),
+  highDefPdtv: XRegExp('hr[-_. ]ws', 'gi'),
+  language: XRegExp('(?:\W|_)\
+    (?<italian>\b(?:ita|italian)\b)|\
+    (?<german>german\b|videomann)|\
+    (?<flemish>flemish)|\
+    (?<greek>greek)|\
+    (?<french>(?:\W|_)(?:FR|VOSTFR)(?:\W|_))|\
+    (?<russian>\brus\b)|\
+    (?<dutch>nl\W?subs?)|\
+    (?<hungarian>\b(?:HUNDUB|HUN)\
+  \b)', 'gix')
+};
+
+class TorrentNameParser {
+  constructor() {
+
+  }
+
+  parse(name) {
+    debug(`Trying to parse torrent name : ${name}.`);
+
+    name = name.trim();
+    const normalizedName = name.replace(/_/g, ' ');
+
+    const infos = {
+      resolution: this.parseResolution(normalizedName),
+      revision: this.parseRevision(normalizedName),
+      language: this.parseLanguage(name)
+      // Continue with this :
+      // https://github.com/Sonarr/Sonarr/blob/develop/src/NzbDrone.Core/Parser/Parser.cs
+      // https://github.com/Sonarr/Sonarr/blob/develop/src/NzbDrone.Core/Parser/ParsingService.cs
+    };
+
+
+    infos.quality = this.parseQuality(name, normalizedName, infos.resolution).name;
+
+    return infos;
+  }
+
+  parseLanguage(name) {
+    /*jshint curly: false */
+    const lowerName = name.toLowerCase();
+
+    if (lowerName.indexOf('english') !== -1)
+      return Language.English;
+
+    if (lowerName.indexOf('french') !== -1)
+      return Language.French;
+
+    if (lowerName.indexOf('spanish') !== -1)
+      return Language.Spanish;
+
+    if (lowerName.indexOf('danish') !== -1)
+      return Language.Danish;
+
+    if (lowerName.indexOf('dutch') !== -1)
+      return Language.Dutch;
+
+    if (lowerName.indexOf('japanese') !== -1)
+      return Language.Japanese;
+
+    if (lowerName.indexOf('cantonese') !== -1)
+      return Language.Cantonese;
+
+    if (lowerName.indexOf('mandarin') !== -1)
+      return Language.Mandarin;
+
+    if (lowerName.indexOf('korean') !== -1)
+      return Language.Korean;
+
+    if (lowerName.indexOf('russian') !== -1)
+      return Language.Russian;
+
+    if (lowerName.indexOf('polish') !== -1)
+      return Language.Polish;
+
+    if (lowerName.indexOf('vietnamese') !== -1)
+      return Language.Vietnamese;
+
+    if (lowerName.indexOf('swedish') !== -1)
+      return Language.Swedish;
+
+    if (lowerName.indexOf('norwegian') !== -1)
+      return Language.Norwegian;
+
+    if (lowerName.indexOf('nordic') !== -1)
+      return Language.Norwegian;
+
+    if (lowerName.indexOf('finnish') !== -1)
+      return Language.Finnish;
+
+    if (lowerName.indexOf('turkish') !== -1)
+      return Language.Turkish;
+
+    if (lowerName.indexOf('portuguese') !== -1)
+      return Language.Portuguese;
+
+    if (lowerName.indexOf('hungarian') !== -1)
+      return Language.Hungarian;
+
+    var match = XRegExp.exec(name, REGEX.language);
+
+    if(match === null) return Language.Unknown;
+    if (match.italian) return Language.Italian;
+    if (match.german) return Language.German;
+    if (match.flemish) return Language.Flemish;
+    if (match.greek) return Language.Greek;
+    if (match.french) return Language.French;
+    if (match.russian) return Language.Russian;
+    if (match.dutch) return Language.Dutch;
+    if (match.hungarian) return Language.Hungarian;
+
+    return Language.English;
+  }
+
+  parseQuality(name, normalizedName, resolution) {
+    if(REGEX.rawHD.test(normalizedName)) {
+      return Quality.RawHD;
+    }
+
+    const sourceMatch = XRegExp.exec(normalizedName, REGEX.source);
+    const codecMatch = XRegExp.exec(normalizedName, REGEX.codec);
+
+    if(sourceMatch !== null) {
+      if (sourceMatch.bluray) {
+        if (codecMatch.xvid || codecMatch.divx) {
+          return Quality.DVD;
+        }
+
+        switch(resolution) {
+          case Resolution._2160p:
+            return Quality.Bluray2160p;
+
+          case Resolution._1080p:
+            return Quality.Bluray1080p;
+
+          case Resolution._480p:
+          case Resolution._576p:
+            return Quality.DVD;
+
+          default:
+            return Quality.Bluray720p;
+        }
+      }
+
+      if (sourceMatch.webdl) {
+        switch(resolution) {
+          case Resolution._2160p:
+            return Quality.WEBDL2160p;
+
+          case Resolution._1080p:
+            return Quality.WEBDL1080p;
+
+          case Resolution._720p:
+            return Quality.WEBDL720p;
+
+          default:
+            if (name.indexOf('[WEBDL]') !== -1) {
+              return Quality.WEBDL720p;
+            }
+
+            return Quality.WEBDL480p;
+        }
+      }
+
+      if (sourceMatch.hdtv) {
+        switch(resolution) {
+          case Resolution._2160p:
+            return Quality.HDTV2160p;
+
+          case Resolution._1080p:
+            return Quality.HDTV1080p;
+
+          case Resolution._720p:
+            return Quality.HDTV720p;
+
+          default:
+            if (name.indexOf('[HDTV]') !== -1) {
+              return Quality.HDTV720p;
+            }
+
+            return Quality.SDTV;
+        }
+      }
+
+      if (sourceMatch.bdrip || sourceMatch.brrip) {
+        switch (resolution) {
+          case Resolution._720p:
+            return Quality.Bluray720p;
+
+          case Resolution._1080p:
+            return Quality.Bluray1080p;
+
+          default:
+            return Quality.DVD;
+        }
+      }
+
+      if (sourceMatch.dvd)
+      {
+        return Quality.DVD;
+      }
+
+      if (sourceMatch.pdtv || sourceMatch.sdtv || sourceMatch.dsr || sourceMatch.tvrip) {
+        if (REGEX.highDefPdtv.test(normalizedName)) {
+          return Quality.HDTV720p;
+        }
+
+        return Quality.SDTV;
+      }
+    }
+
+    switch(resolution) {
+      case Resolution._2160p:
+        return Quality.HDTV2160p;
+
+      case Resolution._1080p:
+        return Quality.HDTV1080p;
+
+      case Resolution._720p:
+        return Quality.HDTV720p;
+
+      case Resolution._480p:
+        return Quality.SDTV;
+    }
+
+    if (codecMatch !== null && codecMatch.x264) {
+      return Quality.SDTV;
+    }
+
+    if (normalizedName.indexOf('848x480') !== -1) {
+      if (normalizedName.indexOf('dvd') !== -1) {
+        return Quality.DVD;
+      }
+
+      return Quality.SDTV;
+    }
+
+    if (normalizedName.indexOf('1280x720') !== -1) {
+      if (normalizedName.indexOf('bluray') !== -1) {
+        return Quality.Bluray720p;
+      }
+
+      return Quality.HDTV720p;
+    }
+
+    if (normalizedName.indexOf('1920x1080') !== -1) {
+      if (normalizedName.indexOf('bluray') !== -1) {
+        return Quality.Bluray1080p;
+      }
+
+      return Quality.HDTV1080p;
+    }
+
+    if (normalizedName.indexOf('bluray720p') !== -1) {
+      return Quality.Bluray720p;
+    }
+
+    if (normalizedName.indexOf('bluray1080p') !== -1) {
+      return Quality.Bluray1080p;
+    }
+
+    var otherSourceMatch = XRegExp.exec(normalizedName, REGEX.otherSource);
+    if (otherSourceMatch !== null)
+    {
+      if(otherSourceMatch.sdtv) {
+        return Quality.SDTV;
+      }
+
+      if(otherSourceMatch.hdtv) {
+        return Quality.HDTV720p;
+      }
+    }
+
+    return Quality.Unknown;
+  }
+
+  parseResolution(name) {
+    /*jshint curly: false */
+    var match = XRegExp.exec(name, REGEX.resolution);
+
+    if(match === null) return Resolution.Unknown;
+    if(match._480p) return Resolution._480p;
+    if(match._576p) return Resolution._576p;
+    if(match._720p) return Resolution._720p;
+    if(match._1080p) return Resolution._1080p;
+    if(match._2160p) return Resolution._2160p;
+  }
+
+  parseRevision(name) {
+    const revision = {
+      version: 1,
+      real: null
+    };
+
+    if(REGEX.proper.test(name)) {
+      revision.version = 2;
+    }
+
+    /*const versionResult = XRegExp.exec(name, REGEX.version);
+    if(versionResult !== null) {
+      revision.version = parseInt(versionResult.version);
+    }*/
+
+    return revision;
+  }
+}
+
+module.exports = TorrentNameParser;
---
+++ test/torrent-name-parser.test.js
@@ -0,0 +1,332 @@
+'use babel';
+
+import chai from 'chai';
+
+import TorrentNameParser from '../src/torrent-name-parser';
+
+const expect = chai.expect;
+
+// See: https://github.com/Sonarr/Sonarr/blob/develop/src/NzbDrone.Core.Test/ParserTests/QualityParserFixture.cs
+
+// Regex ^ *\[TestCase\("([^"]+)", (false|true)\)\]
+const testCases = [
+  {
+    quality: 'SDTV',
+    torrents: [
+      ['S07E23 .avi', false],
+      ['The.Shield.S01E13.x264-CtrlSD', false],
+      ['Nikita S02E01 HDTV XviD 2HD', false],
+      ['Gossip Girl S05E11 PROPER HDTV XviD 2HD', true],
+      ['The Jonathan Ross Show S02E08 HDTV x264 FTP', false],
+      ['White.Van.Man.2011.S02E01.WS.PDTV.x264-TLA', false],
+      ['White.Van.Man.2011.S02E01.WS.PDTV.x264-REPACK-TLA', true],
+      ['The Real Housewives of Vancouver S01E04 DSR x264 2HD', false],
+      ['Vanguard S01E04 Mexicos Death Train DSR x264 MiNDTHEGAP', false],
+      ['Chuck S11E03 has no periods or extension HDTV', false],
+      ['Chuck.S04E05.HDTV.XviD-LOL', false],
+      ['Sonny.With.a.Chance.S02E15.avi', false],
+      ['Sonny.With.a.Chance.S02E15.xvid', false],
+      ['Sonny.With.a.Chance.S02E15.divx', false],
+      ['The.Girls.Next.Door.S03E06.HDTV-WiDE', false],
+      ['Degrassi.S10E27.WS.DSR.XviD-2HD', false],
+      ['[HorribleSubs] Yowamushi Pedal - 32 [480p]', false],
+      ['[CR] Sailor Moon - 004 [480p][48CE2D0F]', false],
+      ['[Hatsuyuki] Naruto Shippuuden - 363 [848x480][ADE35E38]', false],
+      ['Muppet.Babies.S03.TVRip.XviD-NOGRP', false]
+    ]
+  },
+  {
+    quality: 'DVD',
+    torrents: [
+      ['WEEDS.S03E01-06.DUAL.XviD.Bluray.AC3-REPACK.-HELLYWOOD.avi', true],
+      ['The.Shield.S01E13.NTSC.x264-CtrlSD', false],
+      ['WEEDS.S03E01-06.DUAL.BDRip.XviD.AC3.-HELLYWOOD', false],
+      ['WEEDS.S03E01-06.DUAL.BDRip.X-viD.AC3.-HELLYWOOD', false],
+      ['WEEDS.S03E01-06.DUAL.BDRip.AC3.-HELLYWOOD', false],
+      ['WEEDS.S03E01-06.DUAL.BDRip.XviD.AC3.-HELLYWOOD.avi', false],
+      ['WEEDS.S03E01-06.DUAL.XviD.Bluray.AC3.-HELLYWOOD.avi', false],
+      ['The.Girls.Next.Door.S03E06.DVDRip.XviD-WiDE', false],
+      ['The.Girls.Next.Door.S03E06.DVD.Rip.XviD-WiDE', false],
+      ['the.shield.1x13.circles.ws.xvidvd-tns', false],
+      ['the_x-files.9x18.sunshine_days.ac3.ws_dvdrip_xvid-fov.avi', false],
+      ['[FroZen] Miyuki - 23 [DVD][7F6170E6]', false],
+      ['Hannibal.S01E05.576p.BluRay.DD5.1.x264-HiSD', false],
+      ['Hannibal.S01E05.480p.BluRay.DD5.1.x264-HiSD', false],
+      ['Heidi Girl of the Alps (BD)(640x480(RAW) (BATCH 1) (1-13)', false],
+      ['[Doki] Clannad - 02 (848x480 XviD BD MP3) [95360783]', false]
+    ]
+  },
+  {
+    quality: 'WEBDL-480p',
+    torrents: [
+      ['Elementary.S01E10.The.Leviathan.480p.WEB-DL.x264-mSD', false],
+      ['Glee.S04E10.Glee.Actually.480p.WEB-DL.x264-mSD', false],
+      ['The.Big.Bang.Theory.S06E11.The.Santa.Simulation.480p.WEB-DL.x264-mSD', false],
+      ['Da.Vincis.Demons.S02E04.480p.WEB.DL.nSD.x264-NhaNc3', false]
+    ]
+  },
+  {
+    quality: 'HDTV-720p',
+    torrents: [
+      ['Dexter - S01E01 - Title [HDTV]', false],
+      ['Dexter - S01E01 - Title [HDTV-720p]', false],
+      ['Pawn Stars S04E87 REPACK 720p HDTV x264 aAF', true],
+      ['Sonny.With.a.Chance.S02E15.720p', false],
+      ['S07E23 - [HDTV-720p].mkv ', false],
+      ['Chuck - S22E03 - MoneyBART - HD TV.mkv', false],
+      ['S07E23.mkv ', false],
+      ['Two.and.a.Half.Men.S08E05.720p.HDTV.X264-DIMENSION', false],
+      ['Sonny.With.a.Chance.S02E15.mkv', false],
+      ['Gem.Hunt.S01E08.Tourmaline.Nepal.720p.HDTV.x264-DHD', false],
+      ['[Underwater-FFF] No Game No Life - 01 (720p) [27AAA0A0]', false],
+      ['[Doki] Mahouka Koukou no Rettousei - 07 (1280x720 Hi10P AAC) [80AF7DDE]', false],
+      ['[Doremi].Yes.Pretty.Cure.5.Go.Go!.31.[1280x720].[C65D4B1F].mkv', false],
+      ['[HorribleSubs]_Fairy_Tail_-_145_[720p]', false],
+      ['[Eveyuu] No Game No Life - 10 [Hi10P 1280x720 H264][10B23BD8]', false],
+      ['Hells.Kitchen.US.S12E17.HR.WS.PDTV.X264-DIMENSION', false],
+      ['Survivorman.The.Lost.Pilots.Summer.HR.WS.PDTV.x264-DHD', false]
+    ]
+  },
+  {
+    quality: 'HDTV-1080p',
+    torrents: [
+      ['Under the Dome S01E10 Let the Games Begin 1080p', false],
+      ['DEXTER.S07E01.ARE.YOU.1080P.HDTV.X264-QCF', false],
+      ['DEXTER.S07E01.ARE.YOU.1080P.HDTV.x264-QCF', false],
+      ['DEXTER.S07E01.ARE.YOU.1080P.HDTV.proper.X264-QCF', true],
+      ['Dexter - S01E01 - Title [HDTV-1080p]', false],
+      ['[HorribleSubs] Yowamushi Pedal - 32 [1080p]', false]
+    ]
+  },
+  {
+    quality: 'WEBDL-720p',
+    torrents: [
+      ['Arrested.Development.S04E01.720p.WEBRip.AAC2.0.x264-NFRiP', false],
+      ['Vanguard S01E04 Mexicos Death Train 720p WEB DL', false],
+      ['Hawaii Five 0 S02E21 720p WEB DL DD5 1 H 264', false],
+      ['Castle S04E22 720p WEB DL DD5 1 H 264 NFHD', false],
+      ['Chuck - S11E06 - D-Yikes! - 720p WEB-DL.mkv', false],
+      ['Sonny.With.a.Chance.S02E15.720p.WEB-DL.DD5.1.H.264-SURFER', false],
+      ['S07E23 - [WEBDL].mkv ', false],
+      ['Fringe S04E22 720p WEB-DL DD5.1 H264-EbP.mkv', false],
+      ['House.S04.720p.Web-Dl.Dd5.1.h264-P2PACK', false],
+      ['Da.Vincis.Demons.S02E04.720p.WEB.DL.nSD.x264-NhaNc3', false],
+      ['CSI.Miami.S04E25.720p.iTunesHD.AVC-TVS', false],
+      ['Castle.S06E23.720p.WebHD.h264-euHD', false],
+      ['The.Nightly.Show.2016.03.14.720p.WEB.x264-spamTV', false],
+      ['The.Nightly.Show.2016.03.14.720p.WEB.h264-spamTV', false]
+    ]
+  },
+  {
+    quality: 'WEBDL-1080p',
+    torrents: [
+      ['Arrested.Development.S04E01.iNTERNAL.1080p.WEBRip.x264-QRUS', false],
+      ['CSI NY S09E03 1080p WEB DL DD5 1 H264 NFHD', false],
+      ['Two and a Half Men S10E03 1080p WEB DL DD5 1 H 264 NFHD', false],
+      ['Criminal.Minds.S08E01.1080p.WEB-DL.DD5.1.H264-NFHD', false],
+      ['Its.Always.Sunny.in.Philadelphia.S08E01.1080p.WEB-DL.proper.AAC2.0.H.264', true],
+      ['Two and a Half Men S10E03 1080p WEB DL DD5 1 H 264 REPACK NFHD', true],
+      ['Glee.S04E09.Swan.Song.1080p.WEB-DL.DD5.1.H.264-ECI', false],
+      ['The.Big.Bang.Theory.S06E11.The.Santa.Simulation.1080p.WEB-DL.DD5.1.H.264', false],
+      ['Rosemary\'s.Baby.S01E02.Night.2.[WEBDL-1080p].mkv', false],
+      ['The.Nightly.Show.2016.03.14.1080p.WEB.x264-spamTV', false],
+      ['The.Nightly.Show.2016.03.14.1080p.WEB.h264-spamTV', false],
+      ['Psych.S01.1080p.WEB-DL.AAC2.0.AVC-TrollHD', false],
+      ['Series Title S06E08 1080p WEB h264-EXCLUSIVE', false],
+      ['Series Title S06E08 No One PROPER 1080p WEB DD5 1 H 264-EXCLUSIVE', true]
+    ]
+  },
+  {
+    quality: 'WEBDL-2160p',
+    torrents: [
+      ['CASANOVA S01E01.2160P AMZN WEBRIP DD2.0 HI10P X264-TROLLUHD', false],
+      ['JUST ADD MAGIC S01E01.2160P AMZN WEBRIP DD2.0 X264-TROLLUHD', false],
+      ['The.Man.In.The.High.Castle.S01E01.2160p.AMZN.WEBRip.DD2.0.Hi10p.X264-TrollUHD', false],
+      ['The Man In the High Castle S01E01 2160p AMZN WEBRip DD2.0 Hi10P x264-TrollUHD', false],
+      ['The.Nightly.Show.2016.03.14.2160p.WEB.x264-spamTV', false],
+      ['The.Nightly.Show.2016.03.14.2160p.WEB.h264-spamTV', false],
+      ['The.Nightly.Show.2016.03.14.2160p.WEB.PROPER.h264-spamTV', true]
+    ]
+  },
+  {
+    quality: 'Bluray-720p',
+    torrents: [
+      ['WEEDS.S03E01-06.DUAL.Bluray.AC3.-HELLYWOOD.avi', false],
+      ['Chuck - S01E03 - Come Fly With Me - 720p BluRay.mkv', false],
+      ['The Big Bang Theory.S03E01.The Electric Can Opener Fluctuation.m2ts', false],
+      ['Revolution.S01E02.Chained.Heat.[Bluray720p].mkv', false],
+      ['[FFF] DATE A LIVE - 01 [BD][720p-AAC][0601BED4]', false],
+      ['[coldhell] Pupa v3 [BD720p][03192D4C]', false],
+      ['[RandomRemux] Nobunagun - 01 [720p BD][043EA407].mkv', false],
+      ['[Kaylith] Isshuukan Friends Specials - 01 [BD 720p AAC][B7EEE164].mkv', false],
+      ['WEEDS.S03E01-06.DUAL.Blu-ray.AC3.-HELLYWOOD.avi', false],
+      ['WEEDS.S03E01-06.DUAL.720p.Blu-ray.AC3.-HELLYWOOD.avi', false],
+      ['[Elysium]Lucky.Star.01(BD.720p.AAC.DA)[0BB96AD8].mkv', false],
+      ['Battlestar.Galactica.S01E01.33.720p.HDDVD.x264-SiNNERS.mkv', false],
+      ['The.Expanse.S01E07.RERIP.720p.BluRay.x264-DEMAND', true],
+    ]
+  },
+  {
+    quality: 'Bluray-1080p',
+    torrents: [
+      ['Chuck - S01E03 - Come Fly With Me - 1080p BluRay.mkv', false],
+      ['Sons.Of.Anarchy.S02E13.1080p.BluRay.x264-AVCDVD', false],
+      ['Revolution.S01E02.Chained.Heat.[Bluray1080p].mkv', false],
+      ['[FFF] Namiuchigiwa no Muromi-san - 10 [BD][1080p-FLAC][0C4091AF]', false],
+      ['[coldhell] Pupa v2 [BD1080p][5A45EABE].mkv', false],
+      ['[Kaylith] Isshuukan Friends Specials - 01 [BD 1080p FLAC][429FD8C7].mkv', false],
+      ['[Zurako] Log Horizon - 01 - The Apocalypse (BD 1080p AAC) [7AE12174].mkv', false],
+      ['WEEDS.S03E01-06.DUAL.1080p.Blu-ray.AC3.-HELLYWOOD.avi', false],
+      ['[Coalgirls]_Durarara!!_01_(1920x1080_Blu-ray_FLAC)_[8370CB8F].mkv', false],
+    ]
+  },
+  {
+    quality: 'Raw-HD',
+    torrents: [
+      ['POI S02E11 1080i HDTV DD5.1 MPEG2-TrollHD', false],
+      ['How I Met Your Mother S01E18 Nothing Good Happens After 2 A.M. 720p HDTV DD5.1 MPEG2-TrollHD', false],
+      ['The Voice S01E11 The Finals 1080i HDTV DD5.1 MPEG2-TrollHD', false],
+      ['Californication.S07E11.1080i.HDTV.DD5.1.MPEG2-NTb.ts', false],
+      ['Game of Thrones S04E10 1080i HDTV MPEG2 DD5.1-CtrlHD.ts', false],
+      ['VICE.S02E05.1080i.HDTV.DD2.0.MPEG2-NTb.ts', false],
+      ['Show - S03E01 - Episode Title Raw-HD.ts', false],
+      ['Saturday.Night.Live.Vintage.S10E09.Eddie.Murphy.The.Honeydrippers.1080i.UPSCALE.HDTV.DD5.1.MPEG2-zebra', false],
+      ['The.Colbert.Report.2011-08-04.1080i.HDTV.MPEG-2-CtrlHD', false],
+    ]
+  },
+  {
+    quality: null,
+    torrents: [
+      ['Sonny.With.a.Chance.S02E15', false],
+      ['Law & Order: Special Victims Unit - 11x11 - Quickie', false],
+      ['Series.Title.S01E01.webm', false],
+      ['Droned.S01E01.The.Web.MT-dd', false],
+    ]
+  }
+];
+
+describe('TorrentNameParser', () => {
+  const parser = new TorrentNameParser();
+
+  testCases.forEach((testCase) => {
+    testCase.torrents.forEach((torrent) => {
+      const torrentName = torrent[0];
+      const torrentProper = torrent[1];
+
+      const testName = '"' + torrentName + '"';
+      const parsedTorrent = parser.parse(torrentName);
+
+      it(testName, () => {
+        expect(parsedTorrent.quality).to.equal(testCase.quality);
+
+        const version = torrentProper ? 2 : 1;
+        expect(parsedTorrent.revision.version).to.equal(version);
+      });
+    });
+  });
+});
+
+/*const torrents = [
+  {
+    name: 'The Walking Dead S05E03 720p HDTV x264-ASAP[ettv]',
+    resolution: '720p',
+    quality: 'HDTV-720p'
+  },
+  {
+    name: 'Hercules (2014) 1080p BrRip H264 - YIFY',
+    quality: 'HDTV-1080p',
+    resolution: '1080p'
+  },
+  {
+    name: 'The Big Bang Theory S08E06 HDTV XviD-LOL [eztv]',
+    quality: 'SD'
+  },
+  {
+    name: '22 Jump Street (2014) 720p BrRip x264 - YIFY',
+    quality: 'Bluray-720p',
+    resolution: '720p'
+  },
+  {
+    name: 'Hercules.2014.EXTENDED.1080p.WEB-DL.DD5.1.H264-RARBG',
+    quality: 'WEBDL-1080p',
+    resolution: '1080p'
+  },
+  {
+    name: 'Hercules (2014) WEBDL DVDRip XviD-MAX',
+    quality: 'WEBDL-480p'
+  },
+  {
+    name: 'WWE Hell in a Cell 2014 PPV WEB-DL x264-WD -={SPARROW}=-',
+    quality: 'WEBDL-480p'
+  },
+  {
+    name: 'Interstellar (2014) CAM ENG x264 AAC-CPG',
+    quality: 'SDTV'
+  },
+  {
+    name: 'Guardians of the Galaxy (2014) Dual Audio DVDRip AVI',
+    quality: 'DVD'
+  },
+  {
+    name: 'Eliza Graves (2014) Dual Audio WEB-DL 720p MKV x264',
+    quality: 'WEBDL-720p',
+    resolution: '720p'
+  },
+  {
+    name: 'WWE Monday Night Raw 2014 11 10 WS PDTV x264-RKOFAN1990 -={SPARR',
+    quality: 'SDTV'
+  },
+  {
+    name: 'doctor_who_2005.8x12.death_in_heaven.720p_hdtv_x264-fov',
+    quality: 'HDTV-720p',
+    resolution: '720p'
+  },
+  {
+    name: 'breaking.bad.s01e01.720p.bluray.x264-reward',
+    quality: 'Bluray-720p',
+    resolution: '720p'
+  },
+  {
+    name: 'Game of Thrones - 4x03 - Breaker of Chains',
+    resolution: null,
+    quality: null
+  },
+  {
+    name: '[720pMkv.Com]_sons.of.anarchy.s05e10.480p.BluRay.x264-GAnGSteR',
+    resolution: '480p',
+    quality: 'Bluray-480p'
+  },
+  {
+    name: '[ www.Speed.cd ] -Sons.of.Anarchy.S07E07.720p.HDTV.X264-DIMENSION',
+    resolution: '720p',
+    quality: 'HDTV-720p'
+  },
+  {
+    name: 'Community.s02e20.rus.eng.720p.Kybik.v.Kybe',
+    resolution: '720p',
+    language: 'English'
+  }
+];
+
+describe('TorrentNameParser', () => {
+  const parser = new TorrentNameParser();
+
+  torrents.forEach((torrent) => {
+    const testName = '"' + torrent.name + '"';
+    const parsedTorrent = parser.parse(torrent.name);
+
+    it(testName, () => {
+      for(let key in torrent) {
+        if(torrent.hasOwnProperty(key)) {
+          if(key === 'name') {
+            continue;
+          }
+
+          let testMessage = key + ': ' + JSON.stringify(torrent[key]);
+
+          expect(torrent[key], testMessage).to.equal(parsedTorrent[key]);
+        }
+      }
+    });
+  });
+});*/
